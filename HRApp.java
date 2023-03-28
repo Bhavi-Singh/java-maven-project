bc05f3d1c814008ac293a4f778e23ee2584ad3ed
Bhavi-Singh <126875283+Bhavi-Singh@users.noreply.github.com>
2023-03-28 14:19:00 +0530

    Add files via upload

---
+++ HRApp.java
@@ -0,0 +1,35 @@
+package com.bhavi.jms.hr.hrapp;
+
+import javax.jms.JMSContext;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import com.bhavi.jms.hr.Employee;
+
+public class HRApp {
+
+	public static void main(String[] args) throws NamingException {
+		
+		InitialContext context = new InitialContext();
+		Topic empTopic = (Topic) context.lookup("topic/empTopic");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+                JMSContext jmsContext = cf.createContext()){
+			
+			Employee emp = new Employee();
+			emp.setId(100);
+			emp.setFirstName("Bhavi");
+			emp.setLastName("Singh");
+			emp.setDesignation("Intern");
+			emp.setEmail("bhavi@gmail.com");
+			emp.setPhone("9283629291");
+			
+			jmsContext.createProducer().send(empTopic,emp);
+			System.out.println("Message Sent");
+		}
+	}
+
+}
